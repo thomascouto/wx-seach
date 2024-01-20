8ef56ad7a38e5a540c689702036cc260e4bc1341
Thomas Couto (DESKTOP) <thomascouto@gmail.com>
2024-01-20 17:04:36 -0300

    chore: eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,39 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
+  parserOptions: {
+    project: ["./tsconfig.json"],
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['testing-library', '@typescript-eslint', 'react-refresh', 'react'],
+  overrides: [
+    {
+      "files": [
+        "**/__tests__/**/*.[jt]s?(x)",
+        "**/?(*.)+(spec|test).[jt]s?(x)"
+      ],
+      "extends": ["plugin:testing-library/react"]
+    }
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': 'error',
   },
+
 }
