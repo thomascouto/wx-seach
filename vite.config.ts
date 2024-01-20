5c5adf368a3d169d22435361e94e49bc2c802ffa
Thomas Couto (DESKTOP) <thomascouto@gmail.com>
2024-01-20 17:21:15 -0300

    chore: add ts paths to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   clearScreen: true,
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url))
+      }
+    ]
+  }
 })
