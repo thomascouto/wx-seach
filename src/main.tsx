1208ae256243ec668669cd17ef53e6cf3e7c3b31
Thomas Couto (DESKTOP) <thomascouto@gmail.com>
2024-01-21 19:57:04 -0300

    feat: add css classes, organize imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from '@/App';
+
+import './index.css';
+import './normalize.css';
+
+const root = document.getElementById('root');
+
+root &&
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
