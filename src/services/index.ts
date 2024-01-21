06fbdd257b5d972375d1e11bc10b5e95e1d95b3b
Thomas Couto (DESKTOP) <thomascouto@gmail.com>
2024-01-21 19:55:29 -0300

    feat: add weather services

---
+++ src/services/index.ts
@@ -0,0 +1,28 @@
+import { URL as serviceURL } from '@/util/constants';
+
+interface QueryProps {
+  q: string;
+  units?: 'metric' | 'imperial';
+}
+
+async function getWeatherService({ q, units = 'metric' }: QueryProps) {
+  const params = new URLSearchParams({
+    q,
+    units,
+    APPID: import.meta.env.VITE_ACCESS_KEY as string,
+  });
+
+  const request = await fetch(serviceURL + params.toString(), {
+    method: 'GET',
+  });
+
+  const response = (await request.json()) as unknown;
+
+  if (request.ok) {
+    return response as Promise<WX>;
+  }
+
+  throw new Error(JSON.stringify(response));
+}
+
+export { getWeatherService };
