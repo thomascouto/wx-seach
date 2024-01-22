df68b33dbce447e1baf2a07a04b71fc0f520e2f1
Thomas Couto (LAPTOP) <thomascouto@gmail.com>
2024-01-22 00:56:28 -0300

    fix: update imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as TextInput } from './TextInput';
 export { default as Card } from './Card';
+export { default as Spinner } from './Spinner';
+export { default as HistoryCard } from './HistoryCard';
