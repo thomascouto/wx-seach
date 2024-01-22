8298668c1b9092471136eb1d9541fde87efa5ca3
Thomas Couto (LAPTOP) <thomascouto@gmail.com>
2024-01-22 00:56:52 -0300

    feat: add loader to fetch

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -5,8 +5,10 @@ import { getWeatherService } from '@/services';
 export default function useFetch() {
   const [weather, setWeather] = useState<WX>();
   const [error, setError] = useState<ErrorResponse>();
+  const [isLoading, setIsLoading] = useState(false);
 
   const getWeatherData = (q: string) => {
+    setIsLoading(true);
     getWeatherService({
       q,
     })
@@ -14,12 +16,16 @@ export default function useFetch() {
       .catch((err: string) => {
         const parsedError = JSON.parse(err) as ErrorResponse;
         setError(parsedError);
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   };
 
   return {
     getWeatherData,
     weather,
+    isLoading,
     error,
   };
 }
