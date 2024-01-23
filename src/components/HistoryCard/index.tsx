ddc776b6a7f7e3319f76f9e454b79ef1b2ae2d97
Thomas Couto (LAPTOP) <thomascouto@gmail.com>
2024-01-23 00:18:58 -0300

    feat: add history card tooltip, icon, container

--- src/components/HistoryCard/index.tsx
+++ src/components/HistoryCard/index.tsx
@@ -1,15 +1,33 @@
 import clsx from 'clsx';
 
+import { useState } from 'react';
+
+import { getIcon } from '@/util';
+
 import styles from './HistoryCard.module.scss';
 
 interface HistoryCardProps {
-  theme: 'sunny' | 'rainy' | 'storm' | 'windy' | 'snowing';
+  theme: WeatherCondition;
+  location: string;
+  handleClick(): void;
 }
 
-export default function HistoryCard({ theme }: HistoryCardProps) {
+export default function HistoryCard({ theme, handleClick, location }: HistoryCardProps) {
+  const [tooltip, setTooltip] = useState(false);
+
+  const toggleTooltip = () => {
+    setTooltip((prev) => !prev);
+  };
+
   return (
-    <div className={clsx(styles.historyCard, styles[theme])}>
-      <img src={`/historic/${theme}.png`} alt={theme} width="50px" />
-    </div>
+    <button
+      className={clsx(styles.historyCard)}
+      onClick={handleClick}
+      onMouseEnter={toggleTooltip}
+      onMouseLeave={toggleTooltip}
+    >
+      <img src={'/historic/' + getIcon(theme) + '.png'} alt={theme} width="50px" />
+      {tooltip && <span className={styles.tooltip}>{location}</span>}
+    </button>
   );
 }
