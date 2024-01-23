7b3254633a949f9f7cb432a86c2b8562686cd77a
Thomas Couto (LAPTOP) <thomascouto@gmail.com>
2024-01-23 09:06:24 -0300

    feat: update main screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { lazy } from 'react';
+import { lazy, useEffect, useState } from 'react';
 
 import { HistoryCard, Spinner, TextInput } from '@/components';
 import useFetch from '@/hooks/useFetch';
@@ -8,22 +8,49 @@ import styles from './App.module.scss';
 const Card = lazy(() => import('@/components/Card'));
 
 function App() {
-  const { getWeatherData, weather, isLoading } = useFetch();
+  const { getWeatherData, weather, isLoading, error } = useFetch();
+  const [searchHistory, setSearchHistory] = useState<Record<number, WX>>({});
 
-  const handleData = (value: string, units: Units) => {
-    getWeatherData(value);
+  useEffect(() => {
+    if (weather) {
+      setSearchHistory((curr) => ({
+        [weather.id]: weather,
+        ...curr,
+      }));
+    }
+  }, [weather]);
+
+  const handleWeather = (value: string | WX, units?: Units) => {
+    getWeatherData(value, units);
   };
+
+  const handleForecast = () => {
+    console.log('hi');
+  };
+
+  const ErrorMessage = error && <h3>Error! {error.message}</h3>;
+  const FetchedData = weather && <Card data={weather} getForecast={handleForecast} />;
 
   return (
     <>
       <nav className={styles.navigation}>
-        <HistoryCard theme="sunny" />
+        {Object.values(searchHistory).map((wx) => (
+          <HistoryCard
+            key={wx.id}
+            theme={wx.weather[0].main}
+            location={wx.name}
+            handleClick={() => {
+              handleWeather(wx);
+            }}
+          />
+        ))}
       </nav>
+
       <section className={styles.searchBar}>
-        <TextInput action={handleData} />
+        <TextInput action={handleWeather} />
       </section>
       <section className={styles.weatherSection}>
-        {isLoading ? <Spinner /> : weather && <Card data={weather} />}
+        {isLoading ? <Spinner /> : FetchedData ?? ErrorMessage}
       </section>
     </>
   );
