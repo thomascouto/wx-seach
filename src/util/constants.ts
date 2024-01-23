0f68640274d00be30677c9585ef7a61747d03b26
Thomas Couto (DESKTOP) <thomascouto@gmail.com>
2024-01-23 18:51:33 -0300

    feat: add cloud types to constants

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,16 +1,15 @@
 const BASE = 'https://api.openweathermap.org/data/2.5/';
-export const URL_FORECAST = BASE + '/forecast';
-export const URL = BASE + '/weather?';
-
-//?q=fortaleza&units=metric&APPID=99adcb0d8d1c94dc2e7f5457a36cf251
+export const URL_FORECAST = BASE + '/forecast?';
+export const URL_CURRENT = BASE + '/weather?';
 
 export const WEATHER_ICON: Record<WeatherCondition, string> = {
   Clouds: 'cloudy',
   CloudsNight: 'cloudy-night',
   CloudsDay: 'cloudy-day',
   Rain: 'rainy',
+  Drizzle: 'drizzle',
   Snow: 'snowy',
-  Mist: 'snowy',
+  Mist: 'mist',
   ClearNight: 'night',
   ClearDay: 'day',
 };
