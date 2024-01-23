cfeb70b0248fb1820b7a0fb8924fc23f64bdd570
Thomas Couto (DESKTOP) <thomascouto@gmail.com>
2024-01-23 18:52:24 -0300

    refactor: exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export { default as TextInput } from './TextInput';
 export { default as Card } from './Card';
 export { default as Spinner } from './Spinner';
 export { default as HistoryCard } from './HistoryCard';
+export { default as ForecastCard } from './Forecast';
+export { default as ToggleButtons } from './Toggle';
