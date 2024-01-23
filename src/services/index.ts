f0dc97b8a3680b1887dcba76a1ce078c3bbb11cd
Thomas Couto (DESKTOP) <thomascouto@gmail.com>
2024-01-23 18:52:35 -0300

    feat: add forecast url

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,20 +1,22 @@
-import { URL as serviceURL } from '@/util/constants';
+import { URL_CURRENT, URL_FORECAST } from '@/util/constants';
 
-async function getWeatherService({ q, units = 'metric' }: QueryProps) {
+async function getWeatherService<T>({ q, units = 'metric', isForecast }: QueryProps) {
   const params = new URLSearchParams({
     q,
     units,
     APPID: import.meta.env.VITE_ACCESS_KEY as string,
   });
 
-  const request = await fetch(serviceURL + params.toString(), {
+  const url = isForecast ? URL_FORECAST : URL_CURRENT;
+
+  const request = await fetch(url + params.toString(), {
     method: 'GET',
   });
 
   const response = (await request.json()) as unknown;
 
   if (request.ok) {
-    return response as Promise<WX>;
+    return response as Promise<T>;
   }
 
   throw new Error(JSON.stringify(response));
