a8573cc3e9c46743b850fd7ef35851470466020d
Thomas Couto (LAPTOP) <thomascouto@gmail.com>
2024-01-23 00:21:45 -0300

    feat: add icons to constants

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,2 +1,16 @@
-//https://api.openweathermap.org/data/2.5/forecast?q=fortaleza&units=metric&APPID=99adcb0d8d1c94dc2e7f5457a36cf251
-export const URL = 'https://api.openweathermap.org/data/2.5/weather?';
+const BASE = 'https://api.openweathermap.org/data/2.5/';
+export const URL_FORECAST = BASE + '/forecast';
+export const URL = BASE + '/weather?';
+
+//?q=fortaleza&units=metric&APPID=99adcb0d8d1c94dc2e7f5457a36cf251
+
+export const WEATHER_ICON: Record<WeatherCondition, string> = {
+  Clouds: 'cloudy',
+  CloudsNight: 'cloudy-night',
+  CloudsDay: 'cloudy-day',
+  Rain: 'rainy',
+  Snow: 'snowy',
+  Mist: 'snowy',
+  ClearNight: 'night',
+  ClearDay: 'day',
+};
