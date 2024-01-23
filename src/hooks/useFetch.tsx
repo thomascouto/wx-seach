3ace442c39fd7c4ad7f5379c55f4e91f2dad75aa
Thomas Couto (DESKTOP) <thomascouto@gmail.com>
2024-01-23 18:51:49 -0300

    feat: add generics

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -2,21 +2,17 @@ import { useState } from 'react';
 
 import { getWeatherService } from '@/services';
 
-export default function useFetch() {
-  const [weather, setWeather] = useState<WX>();
+export default function useFetch<T>() {
+  const [weather, setWeather] = useState<T>();
   const [error, setError] = useState<ErrorResponse>();
   const [isLoading, setIsLoading] = useState(false);
 
-  const getWeatherData = (q: string | WX, units: Units = 'metric') => {
-    if (typeof q === 'object') {
-      setWeather(q);
-      return;
-    }
-
+  const getWeatherData = ({ q, isForecast, units = 'metric' }: QueryProps) => {
     setIsLoading(true);
-    getWeatherService({
+    getWeatherService<T>({
       q,
       units,
+      isForecast,
     })
       .then(setWeather)
       .catch((err: string) => {
