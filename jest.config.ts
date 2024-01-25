b0a43daad9c76f80427641214367ef466acd93e7
Thomas Couto (LAPTOP) <thomascouto@gmail.com>
2024-01-25 01:20:46 -0300

    chore: add path alias to jest

--- jest.config.ts
+++ jest.config.ts
@@ -97,7 +97,10 @@ const config: Config = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^.+\\.(scss|css)$': 'identity-obj-proxy',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
